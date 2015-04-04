c0978486b77e7be28f002f7891b5dc6f64cfe643
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-04-04 00:46:00 -0300

    Add ergo notifications

--- ergoanimes/apps.py
+++ ergoanimes/apps.py
@@ -3,7 +3,11 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
+
+from . import reports
 
 
 class ErgoAnimesConfig(AppConfig):
@@ -12,3 +16,28 @@ class ErgoAnimesConfig(AppConfig):
     ergo_url = 'animes'
     ergo_url_index = 'ergoanimes:index'
     ergo_verbose_name = _('Animes')
+
+    def ergo_notifications(self, request):
+        html = []
+        reports_url = reverse('ergoanimes:useranime_reports')
+        animes_status = (
+            ('watch', _('Watch'), reports.watch(request.user)),
+            ('down', _('Down'), reports.down(request.user)),
+        )
+        for id_status, status, useranimes in animes_status:
+            count = useranimes.count()
+            if not count:
+                continue
+            text = ['<ul class="no-bullet no-margin">']
+            for useranime in useranimes:
+                if id_status == 'watch':
+                    episodes = '[%s]' % useranime.get_episodes_viewed_display()
+                elif id_status == 'down':
+                    episodes = '[%s]' % useranime.get_episodes_down_display()
+                text.append('<li>%s %s</li>' % (useranime.anime.get_linkdisplay(), episodes))
+            text.append('</ul>')
+            html.append({'url': '%s#%s' % (reports_url, id_status),
+                         'title': '%s: %s' % (self.ergo_verbose_name, status),
+                         'count': count,
+                         'text': mark_safe(''.join(text))})
+        return html
--- ergoanimes/reports.py
+++ ergoanimes/reports.py
@@ -2,35 +2,34 @@
 
 from __future__ import unicode_literals
 
+from django.apps import apps
 from django.db.models import F, Q
-
-from . import models
 
 
 def check_new(user):
-    return models.UserAnime.objects.filter(user=user)\
+    return apps.get_model('ergoanimes', 'UserAnime').objects.filter(user=user)\
         .exclude(status__in=('completed', 'drop'))\
         .exclude(anime__episodes=F('episodes_pub'))\
         .filter(Q(status='watching') | (~Q(status='watching') & Q(episodes_down__isnull=False)))
 
 
 def down(user):
-    return models.UserAnime.objects.filter(user=user)\
+    return apps.get_model('ergoanimes', 'UserAnime').objects.filter(user=user)\
         .exclude(status__in=('completed', 'drop'))\
         .filter(Q(status='watching') | (~Q(status='watching') & Q(episodes_down__isnull=False)))\
         .filter(episodes_down__lt=F('episodes_pub'))
 
 
 def new_down(user):
-    return models.UserAnime.objects.filter(user=user)\
+    return apps.get_model('ergoanimes', 'UserAnime').objects.filter(user=user)\
         .filter(status='new', episodes_pub=F('anime__episodes'))
 
 
 def new_watch(user):
-    return models.UserAnime.objects.filter(user=user)\
+    return apps.get_model('ergoanimes', 'UserAnime').objects.filter(user=user)\
         .filter(status='new', episodes_down=F('anime__episodes'))
 
 
 def watch(user):
-    return models.UserAnime.objects.filter(user=user)\
+    return apps.get_model('ergoanimes', 'UserAnime').objects.filter(user=user)\
         .filter(status__in=('watching', 'hold'), episodes_viewed__lt=F('episodes_down'))
